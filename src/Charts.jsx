83b00d55fc8fd6216d6a7ed2ab0b0d3aa156e0e8
Hardik Nandwani <46419306+Hardiz123@users.noreply.github.com>
2023-05-10 20:44:40 +0530

    Update Charts.jsx

--- src/Charts.jsx
+++ src/Charts.jsx
@@ -1,135 +1,165 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, useEffect, useState } from "react";
 import "./App.css";
 import { PieChart, Pie, Cell } from "recharts";
 import Circle from "./Circle";
 
-const flowData = {
-  nightData: {
-    data: [
-      { name: "active", value: 0, color: "#A396EC" },
-      { name: "inActive", value: 360, color: "#D8D8D8" },
-    ],
-    startAngle: 45,
-    endAngle: 134,
+const backEndData = [
+  {
+    startTime: { hours: 0, minutes: 0 },
+    endTime: { hours: 2, minutes: 30 },
+
   },
-  morningData: {
-    data: [
-      { name: "active", value: 135, color: "#A396EC" },
-      { name: "inActive", value: 225, color: "#D8D8D8" },
-    ],
-    startAngle: -45,
-    endAngle: 44,
+  {
+    startTime: { hours: 8, minutes: 30 },
+    endTime: { hours: 10, minutes: 30 },
   },
-  afternoonData: {
-    data: [
-      { name: "active", value: 0, color: "#A396EC" },
-      { name: "inActive", value: 360, color: "#D8D8D8" },
-    ],
-    startAngle: 225,
-    endAngle: 314,
+  {
+    startTime: { hours: 11, minutes: 30 },
+    endTime: { hours: 12, minutes: 30 },
   },
-  eveningData: {
-    data: [
-      { name: "active", value: 340, color: "#A396EC" },
-      { name: "inActive", value: 0, color: "#D8D8D8" },
-    ],
-    startAngle: 135,
-    endAngle: 224,
+  {
+    startTime: { hours: 15, minutes: 30 },
+    endTime: { hours: 17, minutes: 0 },
   },
+  {
+    startTime: { hours: 17, minutes: 45 },
+    endTime: { hours: 18, minutes: 15 },
+  },
+  {
+    startTime: { hours: 20, minutes: 30 },
+    endTime: { hours: 22, minutes: 30 },
+  },
+  {
+    startTime: { hours: 23, minutes: 0 },
+    endTime: { hours: 24, minutes: 0 },
+  },
+];
+
+const getAngleFromTime = (time) => {
+  const { hours, minutes } = time;
+  const angle = (hours * 60 + minutes) * 0.25;
+  return angle;
 };
+
+
+
 
 const data = [{ name: "active", value: 100, color: "#D8D8D8" }];
 
 export const Charts = () => {
-    return (
-      <div
+  const [angle, setAngle] = useState([])
+
+  useEffect(() => {
+
+    const anglesFromTime = backEndData.map((item) => {
+      const { startTime, endTime } = item;
+      const startAngle = getAngleFromTime(startTime);
+      const endAngle = getAngleFromTime(endTime);
+    
+      return {
+        startAngle,
+        endAngle,
+        value : endAngle - startAngle
+      };
+    });
+
+    let wait = setTimeout(() => {
+      setAngle(anglesFromTime)
+    }, 2000)
+
+    return () => {
+      clearTimeout(wait)
+    }
+  
+  },[])
+  
+  return (
+    <div
+      style={{
+        border: "1px solid #ccc",
+        borderRadius: "20px",
+        boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.2)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "400px",
+        height: "400px",
+        marginLeft: "25%",
+        position: "relative",
+        marginTop: "10%",
+      }}
+    >
+      <div className="heading">Flow Peaks</div>
+      <div className="infoIcon">i</div>
+      <hr className="hrOne" />
+      <hr className="hrTwo" />
+
+      <PieChart
+        width={400}
+        height={400}
         style={{
-          border: "1px solid #ccc",
-          borderRadius: "20px",
-          boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.2)",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "400px",
-          height: "400px",
-          marginLeft: "25%",
-          position: "relative",
-          marginTop: "10%",
+          position: "absolute",
         }}
       >
-        <div className="heading">Flow Peaks</div>
-        <div className="infoIcon">i</div>
-        <hr className="hrOne" />
-        <hr className="hrTwo" />
-
-        <PieChart
-          width={400}
-          height={400}
-          style={{
-            position: "absolute",
-          }}
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          innerRadius={70}
+          outerRadius={85}
+          fill="#D8D8D8"
+          dataKey="value"
+          isAnimationActive={false}
+          cornerRadius={8}
+          strokeOpacity={0}
+          direction={"clockwise"}
         >
-          <Pie
-            data={data}
-            cx={200}
-            cy={200}
-            innerRadius={70}
-            outerRadius={85}
-            fill="#D8D8D8"
-            dataKey="value"
-            isAnimationActive={false}
-            cornerRadius={8}
-            strokeOpacity={0}
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={entry.color} />
-            ))}
-          </Pie>
-        </PieChart>
-        <div style={{ position: "absolute", left: "55px", fontWeight: "bold" }}>
-          Evening
-        </div>
-        <div style={{ position: "absolute", top: "90px", fontWeight: "bold" }}>
-          Night
-        </div>
-        <div
-          style={{ position: "absolute", right: "45px", fontWeight: "bold" }}
-        >
-          Morning
-        </div>
-        <div
-          style={{ position: "absolute", bottom: "80px", fontWeight: "bold" }}
-        >
-          Afternoon
-        </div>
-        <PieChart width={400} height={400}>
-          {Object.keys(flowData).length && Object?.keys(flowData).map((key, index) => {
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={entry.color} />
+          ))}
+        </Pie>
+      </PieChart>
+      <div style={{ position: "absolute", left: "55px", fontWeight: "bold" }}>
+        Evening
+      </div>
+      <div style={{ position: "absolute", top: "90px", fontWeight: "bold" }}>
+        Night
+      </div>
+      <div style={{ position: "absolute", right: "45px", fontWeight: "bold" }}>
+        Morning
+      </div>
+      <div style={{ position: "absolute", bottom: "80px", fontWeight: "bold" }}>
+        Afternoon
+      </div>
+      <PieChart width={400} height={400}>
+        {
+        angle.map((key, index) => {
+          console.log(index, "this is index");
             return (
               <Pie
-                data={flowData[key].data}
+                data={[key]}
                 cx={200}
                 cy={200}
-                startAngle={flowData[key].startAngle}
-                endAngle={flowData[key].endAngle}
+                startAngle={-(270+key.startAngle)}
+                endAngle={-(270+key.endAngle)}
                 innerRadius={70}
+                
+                // dataKey="value"
+                direction={"anticlockwise"}
                 outerRadius={85}
+                valueKey={"value"}
                 fill="#8884d8"
                 isAnimationActive={false}
-                dataKey="value"
                 strokeOpacity={0}
                 cornerRadius={8}
-                key={"chart"+index}
-              >
-                {flowData[key].data.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={entry.color} />
-                ))}
-              </Pie>
+                key={"chart" + index}
+              />
             );
           })}
-        </PieChart>
+      </PieChart>
 
-        <Circle radius={90} color="" id="circleContainer" />
-        <button className="buttonClass">April</button>
-      </div>
-    );
-}
+      <Circle radius={90} color="" id="circleContainer" />
+      <button className="buttonClass">April</button>
+    </div>
+  );
+};
